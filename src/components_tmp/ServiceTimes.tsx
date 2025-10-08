cbb087e2385e4fd17458bb5144bac3d496950eb5
stephalucas <stephanc888.sc@gmail.com>
2025-10-08 13:42:18 +0200

    Rename Components -> components_tmp

---
+++ src/components_tmp/ServiceTimes.tsx
@@ -0,0 +1,11 @@
+export default function ServiceTimes() {
+  return (
+    <section id="dienste" className="mt-12">
+      <h2 className="text-2xl font-semibold mb-4">Dienste Tye</h2>
+      <ul className="space-y-2">
+        <li>Sondag Oggend: 09:00</li>
+        <li>Woensdagaand Bybelstudie: 19:00</li>
+      </ul>
+    </section>
+  );
+}
