49ec482d80575580c07c81ba99ac3ad4452fa9a9
stephalucas <stephanc888.sc@gmail.com>
2026-02-11 16:59:27 +0200

    Sel Update

--- src/components/BekeerlingeCounter.tsx
+++ src/components/BekeerlingeCounter.tsx
@@ -5,7 +5,7 @@ import confetti from "canvas-confetti";
 import { motion } from "framer-motion";
 
 export default function BekeerlingeCounter() {
-  const souls =90;
+  const souls =111;
   const [showCelebration, setShowCelebration] = useState(false);
   const [showFloating, setShowFloating] = useState(false);
   const [displayCount, setDisplayCount] = useState(0);
